ac02e69d6df7023a57d6da0c923239d85854eb0f
Kasper Møller Hald <kasper@17b.dk>
2013-11-15 11:42:56 +0100

    0.2.4

--- Flow 4/src/flow/pkg4/WordPairControl.java
+++ Flow 4/src/flow/pkg4/WordPairControl.java
@@ -1,6 +1,20 @@
 package flow.pkg4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class WordPairControl implements WordPairControlIF {
+    
+    private static PrintWriter pw;
+    private static StringBuilder strBuilder = new StringBuilder();
+    private static Random r = new Random();
+    private static Scanner s;
+    private static ArrayList<String[]> wordList = new ArrayList<>();
+    private String path;
 
     @Override
     public void add(String question, String answer) {
@@ -34,12 +48,65 @@ public class WordPairControl implements WordPairControlIF {
 
     @Override
     public boolean load() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        strBuilder.append(System.getProperty("user.dir"));
+        strBuilder.append("/wordlist");
+        String dir = strBuilder.toString();
+        File file;
+        try {
+            file = new File(dir);
+            if (!file.exists()) {
+                file.mkdir();
+                this.path = dir;
+            } else {
+                this.path = dir;
+            }
+            return buildList();
+        } catch (Exception e) {
+        }
+        try {
+            strBuilder.append("/wordlist.txt");
+            file = new File(strBuilder.toString());
+            file.createNewFile();
+            this.path = strBuilder.toString();
+            return buildList();
+        } catch (Exception e) {
+            return false;
+        }
     }
+    
+    private boolean buildList(){
+        try {
+            wordList = new ArrayList();
+            s = new Scanner(new File(path));
+            while (s.hasNext()) {
+                String tmp = s.nextLine();
+                String[] words = tmp.split(",");
+                wordList.add(words);
+            }
+            return true;
+        } catch (FileNotFoundException ex) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    } 
 
     @Override
     public boolean save() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            pw = new PrintWriter(path);
+            for (int x = 0; x < wordList.size(); ++x) {
+                String[] temp = wordList.get(x);
+                pw.write(temp[0] + "," + temp[1]);
+                pw.println("\n");
+            }
+            pw.close();
+            return true;
+        } catch (FileNotFoundException ex) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
